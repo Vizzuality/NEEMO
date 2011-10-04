90d1426ebb76b5a46041cec705aba04a2da824ce
Javier Arce <javierarce@gmail.com>
2011-10-04 13:48:57 +0200

    The open/close status of the selector is controlled via a class name

--- app/public/js/radial_selector.js
+++ app/public/js/radial_selector.js
@@ -14,13 +14,16 @@
   sectorPath = "M230.301,135.651c3.62,8.709,3.48,18.071,0.301,26.23l93.729,36.011c12.09-31.82,11.98-68.321-3.16-101.931l-0.21-0.46L230.301,135.651z",
   sectorOpacity = .40,
   sectorOpacityDisabled = .25,
-  sectorOpacityActive = .5;
+  sectorOpacityActive = .5,
+  selectedOption;
 
-  var _open = false;
 
 $(function() {
 
-  var svg = Raphael("radial_selector", canvasWidth, canvasHeight);
+  var selectorID = "radial_selector";
+  var $radial_selector = $("#"+ selectorID);
+
+  var svg = Raphael(selectorID, canvasWidth, canvasHeight);
   var circle = svg.circle(centerX, centerY, 0).attr({ stroke:"none", fill: "r(0.75, 0.75)#000-#333" });
 
   /* Options for the radial selector */
@@ -44,6 +47,9 @@ $(function() {
   function selectOption(name, e) {
     e.preventDefault();
     e.stopPropagation();
+
+    selectedOption = name;
+
     // TODO: add click event
   }
 
@@ -56,13 +62,16 @@ $(function() {
       option.translate(options[i].cX, options[i].cY);
       option.rotate(options[i].angle);
 
-      var sectorSVG = svg.path(sectorPath).attr({ cursor:"hand", fill: "#000", stroke: "none", opacity: sectorOpacity });
-      sectorSVG.hover(onFocusSector, onBlurSector);
+      var attr = { cursor:"hand", fill: "#000", stroke: "none", opacity: sectorOpacity };
 
+      var sectorSVG = svg.path(sectorPath).attr(attr);
+      sectorSVG.hover(onFocusSector, onBlurSector);
       sectorSVG.click(options[i].action);
 
     } else {
-      var sectorSVG = svg.path(sectorPath).attr({ fill: "#000", stroke: "none", opacity: sectorOpacityDisabled });
+      var attr = { fill: "#000", stroke: "none", opacity: sectorOpacityDisabled };
+      var sectorSVG = svg.path(sectorPath).attr(attr);
+
       sectorSVG.click(function(e) {
         e.preventDefault();
         e.stopPropagation();
@@ -80,9 +89,9 @@ $(function() {
     e.preventDefault();
     e.stopPropagation();
 
-    _open = false;
+    $radial_selector.removeClass("open");
 
-    $("#radial_selector").delay(500).animate({ opacity: 0 });
+    $radial_selector.delay(500).animate({ opacity: 0 });
 
     circle.animate({r:0}, 1000, '<>');
     for (i = 0; i <= sectorNum - 1; i++) {
@@ -91,13 +100,11 @@ $(function() {
   }
 
   function toggleRadialSelector(e) {
-    if (!_open) {
-      _open = true;
+    if (!$radial_selector.hasClass("open")) {
 
-      $("#radial_selector").css({ left: e.clientX - 133, top: e.clientY - 133 }).animate({ opacity: 1 });
+      $radial_selector.css({ left: e.clientX - 133, top: e.clientY - 133 }).animate({ opacity: 1 });
 
       circle.animate({opacity:.7, r:30}, 1000, '<>');
-
 
       for (i = 0; i <= sectorNum - 1; i++) {
         $(sectors[i].option.node).delay(1000).animate( {opacity: 1 });
@@ -105,13 +112,14 @@ $(function() {
           rotation: (360 - 45 * i) + " " + centerX + " " + centerY
         }, 1000, '<>');
       }
+      $radial_selector.addClass("open");
 
     } else {
       closeRadialSelector(e);
     }
   }
 
-  $("#radial_selector").click(toggleRadialSelector);
+  $radial_selector.click(toggleRadialSelector);
   $(".photo").click(toggleRadialSelector);
 
 });
