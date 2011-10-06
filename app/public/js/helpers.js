bda3c4dec42de1bcb0e82cfb3d30186c04e8abee
Javier Arce <javierarce@gmail.com>
2011-10-06 19:04:43 +0200

    Adds shortcuts & help

--- app/public/js/helpers.js
+++ app/public/js/helpers.js
@@ -1,4 +1,147 @@
 /*
+* GOD sees everything
+*/
+var GOD = (function() {
+  var subscribers = {};
+  var debug = true;
+
+  function unsubscribe(event) {
+    debug && console.log("Unsubscribe ->", event);
+    delete subscribers[event];
+  }
+
+  function subscribe(event) {
+    debug && console.log("Subscribe ->", event);
+
+    subscribers[event] = event
+  }
+
+  function _signal(event) {
+    debug && console.log("Signal to ", event);
+
+    $(window).trigger(event);
+    unsubscribe(event);
+  }
+
+  function _signalAll() {
+    if (!_.isEmpty(subscribers)) {
+      _.each(subscribers, _signal);
+    }
+  }
+
+  // send signal to all the other subscribers
+  function broadcast(protectedEvent) {
+    _.each(subscribers, function(event) {
+      //protectedEvent != event && _signal(event);
+    });
+  }
+
+  $(function() {
+    $(document).keyup(function(e) {
+      e.keyCode == 27 && _signalAll();
+    });
+
+    $('html').click(_signalAll);
+  });
+
+  return {
+    subscribe: subscribe,
+    unsubscribe: unsubscribe,
+    broadcast: broadcast
+  };
+})();
+
+function _toggleLockScreen(callback) {
+  var $lock_screen = $("#lock_screen");
+
+  if ($lock_screen.length) {
+    $lock_screen.fadeOut(150, function() { $(this).remove(); });
+  } else {
+    $("body").append("<div id='lock_screen'></div>");
+    $("#lock_screen").height($(document).height());
+    $("#lock_screen").fadeIn(150, function() {
+      callback && callback();
+    });
+  }
+}
+
+jQuery.fn.helpShortcuts = function(opt) {
+
+  var $el,
+  id = "helpShortcuts",
+  speed  = (opt && opt.speed) || 200,
+  easingMethod = (opt && opt.easingMethod) || "easeOutExpo",
+  $popover = $(".shortcuts");
+
+  $(window).bind('_close.' + id, function() {
+    _close($popover);
+  });
+
+  function _close(el) {
+    GOD.unsubscribe("_close." + id);
+    el.fadeOut(speed);
+    _toggleLockScreen();
+  }
+  this.each(function() {
+    $el = $(this);
+
+    var event = "_close.help";
+
+    $popover.find(".close").click(function() {
+      _close($popover);
+    });
+
+    $el.click(function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      $popover.fadeIn(speed);
+    _toggleLockScreen();
+      GOD.subscribe("_close." + id);
+      GOD.broadcast("_close." + id);
+    });
+  });
+}
+
+jQuery.fn.helpInfo = function(opt) {
+
+  var $el,
+  panelWidth = 485,
+  speed  = (opt && opt.speed) || 200,
+  easingMethod = (opt && opt.easingMethod) || "easeOutExpo";
+
+  function updateHeader(name) {
+    $el.find("h3").html("What is a " + name + "?");
+  }
+
+  this.each(function() {
+    $el = $(this);
+
+    $el.find(".close").click(function() {
+      $el.fadeOut(speed);
+    });
+
+    $el.find(".more-info, .nav").click(function() {
+      var species = $(this).parents("li").next("li");
+      var direction = "+=";
+      var distance = panelWidth;
+
+      if ($(this).hasClass("previous")) {
+        species = $(this).parents("li").prev("li");
+        direction = "-=";
+      } else if ($(this).hasClass("first")) {
+        species = $(this).parents("ul").find("li:first-child");
+        direction = 0;
+        distance = 0;
+      }
+
+      $el.find(".inner").scrollTo(direction + distance, speed, {easing:easingMethod} );
+      var speciesName = species.find(".icon").attr("class").replace("icon ", "");
+      updateHeader(speciesName);
+    });
+  });
+}
+
+/*
 * Controls the behaviour of the navigation menu
 * */
 jQuery.fn.navigationHover = function(opt) {
@@ -30,7 +173,7 @@ jQuery.fn.navigationHover = function(opt) {
       setTimeout(sel, 200);
     }
 
-    $(this).parent().mouseleave(function(e){
+    $(this).mouseleave(function(e){
       select($currentOption);
     });
 
