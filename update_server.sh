e362f83d99b94b94e5f4ff3fb00817e4c9122084
Andrew W Hill <andrewxhill@gmail.com>
2011-10-10 15:59:18 -0400

    sh for remote

--- update_server.sh
+++ update_server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ssh -i ~/Dropbox/ec2-keys/id-vizzuality ubuntu@neemo.cartodb.com 'cd NEEMO/ && git pull && cd app && sudo killall node && sudo node app.js production > update.log &'
+ssh -i ~/Dropbox/ec2-keys/id-vizzuality ubuntu@neemo.cartodb.com 'cd NEEMO && git pull && cd app && sudo killall node > /dev/null && nohup sudo node app.js production > app.log &'
