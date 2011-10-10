12d5570ecc06c3ab258cc4220a3ef2d23e9d0714
Andrew W Hill <andrewxhill@gmail.com>
2011-10-10 17:09:29 -0400

    update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-clear
 cd /home/ubuntu/NEEMO/
 exec git pull
 cd /home/ubuntu/NEEMO/app
 exec sudo killall node
-exec sudo -u ubuntu /usr/local/bin/node /home/ubuntu/NEEMO/app.js 2>&1 >> /var/log/node.log
+exec sudo -u Linux /usr/local/bin/node /home/ubuntu/NEEMO/app.js 2>&1 >> /var/log/node.log
 sleep 1
 exit
 
