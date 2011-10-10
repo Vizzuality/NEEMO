aff48a90c6de4318279a5ef9ee0d13a5336c9c75
Javier de la Torre <jatorre@gmail.com>
2011-10-10 15:25:51 -0400

    first try to a deploy script

--- update_server.sh
+++ update_server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ssh -i ~/Dropbox/ec2-keys/id-vizzuality ubuntu@neemo.cartodb.com 'cd NEEMO/ && git pull'
+ssh -i ~/Dropbox/ec2-keys/id-vizzuality ubuntu@neemo.cartodb.com 'cd NEEMO/ && git pull && cd app && sudo killall node && sudo node app.js production > update.log &'
