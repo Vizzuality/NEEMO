e9bbf69a1a163b3191a48a075253e4241aa445bd
Andrew W Hill <andrewxhill@gmail.com>
2011-10-10 16:39:59 -0400

    update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 cd /home/ubuntu/NEEMO/
 git pull
 cd /home/ubuntu/NEEMO/app
-sudo killall node > /dev/null
+sudo killall node > /dev/null 2>&1 
 sudo node app.js production 
 cd /home/ubuntu/NEEMO/
 
--- update_server.sh
+++ update_server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ssh -i ~/Dropbox/ec2-keys/id-vizzuality ubuntu@neemo.cartodb.com '/home/ubuntu/NEEMO/update.sh'
+ssh -i ~/Dropbox/ec2-keys/id-vizzuality ubuntu@neemo.cartodb.com '/home/ubuntu/NEEMO/update.sh > /home/ubuntu/NEEMO/app.log'
