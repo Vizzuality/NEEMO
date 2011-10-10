56608b46748dd2ea295ef299f4722ccd7cb97329
Andrew W Hill <andrewxhill@gmail.com>
2011-10-10 16:31:16 -0400

    update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 cd /home/ubuntu/NEEMO/
-`git pull`
+git pull
 cd /home/ubuntu/NEEMO/app
-`sudo killall node > /dev/null`
-`nohup sudo node app.js production &`
+sudo killall node > /dev/null
+nohup sudo node app.js production &
+
+cd /home/ubuntu/NEEMO/
+
+
