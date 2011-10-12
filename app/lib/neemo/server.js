ba4c0054e776ed9b88561334255382da929256d3
Andrew W Hill <andrewxhill@gmail.com>
2011-10-12 13:23:03 -0400

    removed login redirect

--- app/lib/neemo/server.js
+++ app/lib/neemo/server.js
@@ -54,15 +54,6 @@ module.exports = function(){
         } else if (route == '/login' ){
             res.redirect(csa.login + '?service=' + csa.service);
         } else if (req.session && req.session.loggedin){
-            if (ticket) {
-              cas.validate(ticket, function(err, status, username) {
-                  if (err) {
-                     res.redirect('/');
-                  } else{
-                     res.redirect('/mission.html');
-                  }
-              });
-            }
             next();
         } else {
             if (ticket) {
