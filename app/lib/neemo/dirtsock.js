3fbd7bf7b96fceedef10aeb10112f71643893a33
Andrew W Hill <andrewxhill@gmail.com>
2011-10-12 12:44:33 -0400

    flash transport test

--- app/lib/neemo/dirtsock.js
+++ app/lib/neemo/dirtsock.js
@@ -33,7 +33,7 @@ exports.start = function(io, cartodb, store) {
     });
     */
     io.configure(function () {
-      io.set('transports', ['websocket', 'xhr-polling']);
+      io.set('transports', ['flashsocket', 'xhr-polling']);
     });
     io.sockets.on('connection', function (socket) {
         var session_id = socket.id;
