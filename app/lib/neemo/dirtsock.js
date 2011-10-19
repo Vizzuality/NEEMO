d7bee7138a55255e49973dccf496ca952284c57c
Simon Tokumine <simon@japancentre.com>
2011-10-19 18:31:08 +0100

    update dirtsock to have production settings as per docs

--- app/lib/neemo/dirtsock.js
+++ app/lib/neemo/dirtsock.js
@@ -33,7 +33,17 @@ exports.start = function(io, cartodb, store) {
     });
     */
     io.configure(function () {
-      io.set('transports', ['xhr-polling']);
+      io.enable('browser client minification');  // send minified client
+      io.enable('browser client etag');          // apply etag caching logic based on version number
+      io.enable('browser client gzip');          // gzip the file
+      io.set('log level', 1);                    // reduce logging
+      io.set('transports', [                     // enable all transports (optional if you want flashsocket)
+          'websocket'
+        , 'flashsocket'
+        , 'htmlfile'
+        , 'xhr-polling'
+        , 'jsonp-polling'
+      ]);
     });
     io.sockets.on('connection', function (socket) {
         var session_id = socket.id;
