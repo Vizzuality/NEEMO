643d9a313e7594b61db9c7d7717875dd53d6efad
Simon Tokumine <simon@japancentre.com>
2011-10-19 18:39:30 +0100

    revert

--- app/lib/neemo/dirtsock.js
+++ app/lib/neemo/dirtsock.js
@@ -33,17 +33,7 @@ exports.start = function(io, cartodb, store) {
     });
     */
     io.configure(function () {
-      io.enable('browser client minification');  // send minified client
-      io.enable('browser client etag');          // apply etag caching logic based on version number
-      io.enable('browser client gzip');          // gzip the file
-      io.set('log level', 1);                    // reduce logging
-      io.set('transports', [                     // enable all transports (optional if you want flashsocket)
-          'websocket'
-        , 'flashsocket'
-        , 'htmlfile'
-        , 'xhr-polling'
-        , 'jsonp-polling'
-      ]);
+      io.set('transports', ['xhr-polling']);
     });
     io.sockets.on('connection', function (socket) {
         var session_id = socket.id;
